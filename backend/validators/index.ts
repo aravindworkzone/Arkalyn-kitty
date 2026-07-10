df46119231d894cfacdc83fcbfd4393126572666
Aravind A <aravind.workzone@gmail.com>
2026-07-10 16:52:33 +0530

    Remove dead code, debug scripts, and unused dependencies

--- backend/validators/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './common';
-export * from './auth.validator';
-export * from './category.validator';
-export * from './group.validator';
-export * from './expense.validator';
--- frontend/src/helpers/validation.ts
+++
@@ -1,5 +0,0 @@
-export { MAX_AMOUNT as MaxAmount, handleAmountInput as validateAmount, validateGroupName as validateHeader } from "./validators";
-import { validateEmail as _validateEmail } from "./validators";
-
-export const validateEmail = (email: string): boolean =>
-  _validateEmail(email).valid;
